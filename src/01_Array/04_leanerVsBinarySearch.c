e6a3c022b9cfc6028a01e07738cb5d52c7970b70
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-05-21 19:18:57 +0600

    feat[array]: binary search algorithm implemented

--- src/01_Array/04_leanerVsBinarySearch.c
+++ src/01_Array/04_leanerVsBinarySearch.c
@@ -11,12 +11,40 @@ int linearSearch(int arr[], int arrSize, int element)
     }
     return -1;
 }
+int binarySearch(int arr[], int arrSize, int element)
+{
+    int low = 0, high = arrSize, mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (arr[mid] == element)
+        {
+            return mid;
+        }
+        if (arr[mid] < element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
     int numbers[] = {20, 30, 45, 50, 70, 84, 90, 50, 54, 69, 42, 25, 60, 44, 55};
     int size = sizeof(numbers) / sizeof(int), element = 55;
 
-    int searchResult = linearSearch(numbers, size, element);
-    printf("Element found at index: %d\n", searchResult);
+    int linearSearchResult = linearSearch(numbers, size, element);
+    printf("Element found at index: %d\n", linearSearchResult);
+
+    int sortedNumber[] = {5, 10, 15, 20, 25, 30};
+    int nSize = sizeof(sortedNumber) / sizeof(int), searchElement = 15;
+    int binarySearchResult = binarySearch(sortedNumber, nSize, searchElement);
+    printf("Element found at index: %d\n", binarySearchResult);
+
     return 0;
 }
