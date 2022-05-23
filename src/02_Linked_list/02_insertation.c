da043835666924f7eb516607fe640d05e7d80380
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-05-23 19:12:12 +0600

    feat[linked-list]: insertion methood 1

--- src/02_Linked_list/02_insertation.c
+++ src/02_Linked_list/02_insertation.c
@@ -5,9 +5,16 @@ int main()
 
     struct Node *head = (struct Node *)malloc(sizeof(struct Node));
     struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *fourth = (struct Node *)malloc(sizeof(struct Node));
 
     assignValue(head, 44, second);
-    assignValue(second, 50, NULL);
+    assignValue(second, 55, third);
+    assignValue(third, 66, fourth);
+    assignValue(fourth, 77, NULL);
+    traverseLinkedList(head);
+
+    head = insertAtFirst(head, 56);
     traverseLinkedList(head);
     return 0;
 }
--- src/02_Linked_list/linked_list.c
+++ src/02_Linked_list/linked_list.c
@@ -17,7 +17,18 @@ extern void traverseLinkedList(struct Node *head)
 {
     while (head != NULL)
     {
-        printf("%d\n", head->data);
+        printf("%d ", head->data);
         head = head->next;
     }
+    printf("\n");
+}
+
+// Insertion:
+// Case 1: Insert at first
+extern struct Node *insertAtFirst(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr;
 }
