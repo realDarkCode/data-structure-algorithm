d7971bcfbc2b57c37f7b7263d237ac2787fb2660
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-05-27 12:03:54 +0600

    feat[stack]: isEmpty, isFull status of a stack

--- src/03_stack/01_implementing_stack.c
+++ src/03_stack/01_implementing_stack.c
@@ -3,5 +3,15 @@
 int main()
 {
     struct STACK *s = (struct STACK *)malloc(sizeof(struct STACK));
-    createStack(s, 20);
+    createStack(s, 2);
+
+    checkEmptyStatus(s);
+
+    // Pushing values manually
+    s->arr[0] = 55;
+    s->top++;
+    s->arr[1] = 66;
+    s->top++;
+
+    checkFullStatus(s);
 }
--- src/03_stack/stack.c
+++ src/03_stack/stack.c
@@ -15,3 +15,44 @@ extern void createStack(struct STACK *st, int size)
     st->top = -1;
     st->arr = (int *)malloc(sizeof(int) * st->size);
 }
+
+extern int isEmpty(struct STACK *sPtr)
+{
+    if (sPtr->top == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+extern int isFull(struct STACK *sPtr)
+{
+    if (sPtr->top >= sPtr->size - 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+extern void checkEmptyStatus(struct STACK *sPtr)
+{
+    if (isEmpty(sPtr))
+    {
+        printf("The stack is empty\n");
+    }
+    else
+    {
+        printf("The stack is not empty\n");
+    }
+}
+extern void checkFullStatus(struct STACK *sPtr)
+{
+    if (isFull(sPtr))
+    {
+        printf("The stack is full\n");
+    }
+    else
+    {
+        printf("The stack is not full\n");
+    }
+}
